2a0f5deff2e5e253ea0171596ba7e7c89fb4c9a9
Scott Mertz <scott@cyngn.com>
2016-05-25 15:47:42 -0700

    debuggerd: add euclid version to tombstones
    Change-Id: I7822a2e133a2f326ad2d8da8f79b0064344d59bf
    
    debuggerd: Rebrand tombstone header to LineageOS
    
    [harryyoud]: Use new props for 15.1
    [rashed]: Bah... use GetProperty
    [mkbestas]: Rewrite for T
    
    Change-Id: Idd08c2eb7e395b464b1510742bf52833f465db08

--- debuggerd/libdebuggerd/tombstone_proto_to_text.cpp
+++ debuggerd/libdebuggerd/tombstone_proto_to_text.cpp
@@ -581,6 +581,7 @@ static void print_guest_thread(CallbackType callback, SymbolizeCallbackType symb
 bool tombstone_proto_to_text(const Tombstone& tombstone, CallbackType callback,
                              SymbolizeCallbackType symbolize) {
   CBL("*** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***");
+  CBL("EuclidOS Version: '%s'", tombstone.euclid_version().c_str());
   CBL("Build fingerprint: '%s'", tombstone.build_fingerprint().c_str());
   CBL("Revision: '%s'", tombstone.revision().c_str());
   CBL("ABI: '%s'", abi_string(tombstone.arch()));
